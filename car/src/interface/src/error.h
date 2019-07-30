758b6efeda7104890f20c0494357da018fd4c2bc
Liu Yu <source@liuyu.com>
2019-07-30 19:45:49 +0800

    add schema validator for signaling: login, login resp, logout, logout resp, ping, pong

---
+++ car/src/interface/src/error.h
@@ -0,0 +1,18 @@
+namespace roscar
+{
+namespace car
+{
+namespace interface
+{
+
+static const int SUCCESS = 0;
+static const int ERROR_SUCCESS = SUCCESS;
+static const int FAIL = -1;
+static const int NEED_MORE_DATA = -2;
+static const int ERROR = FAIL;
+static const int ERROR_INVALID = -10;
+static const int ERROR_UNSUPPORT = -11;
+
+} // namespace interface
+} // namespace car
+} // namespace roscar
--- car/src/interface/src/rcmp.h
+++ car/src/interface/src/rcmp.h
@@ -1,6 +1,11 @@
 #ifndef _ROSCAR_CAR_INTERFACE_RCMP_H_
 #define _ROSCAR_CAR_INTERFACE_RCMP_H_
 
+#include <assert.h>
+
+#include "rapidjson/document.h"
+#include "rapidjson/schema.h"
+
 namespace roscar
 {
 namespace car
@@ -17,14 +22,87 @@ public:
     static const int RCMP_MIN_FRAME_SIZE = RCMP_FRAMEHEADSIZE;
     static const int RCMP_MAXPAYLOAD = 0x10000 - RCMP_FRAMEHEADSIZE;
 
+    static const char *FIELD_CMD;
+    static const char *FIELD_SEQ;
+    static const char *FIELD_PAYLOAD;
+    static const char *FIELD_LOGIN_VER;
+    static const char *FIELD_LOGIN_TYPE;
+    static const char *FIELD_LOGIN_ID;
+
+    static const char *SIG_LOGIN;
+    static const char *SIG_LOGIN_RESP;
+    static const char *SIG_LOGOUT;
+    static const char *SIG_LOGOUT_RESP;
+    static const char *SIG_PING;
+    static const char *SIG_PONG;
+    static const char *SIG_CONTROL;
+    static const char *SIG_CONTROL_RESP;
+    static const char *SIG_MT;
+    static const char *SIG_MT_RESP;
+    static const char *SIG_REPORT;
+    static const char *SIG_REPORT_RESP;
+
+    static const char *SCHEMA_SIG;
+    static const char *SCHEMA_SIG_LOGIN;
+    static const char *SCHEMA_SIG_LOGIN_RESP;
+    static const char *SCHEMA_SIG_LOGOUT;
+    static const char *SCHEMA_SIG_LOGOUT_RESP;
+    static const char *SCHEMA_SIG_PING;
+    static const char *SCHEMA_SIG_PONG;
+
     typedef struct FRAME
     {
         unsigned char startFlag;
         unsigned char reserved;
-        unsigned short length;
-        char * payload;
+        unsigned char length[2];
+        char *payload;
+
+        void setLen(int len)
+        {
+            assert(!(len & 0xFFFF0000));
+            length[0] = len >> 8;
+            length[1] = len & 0xFF;
+        }
+        int len()
+        {
+            return (length[0] << 8) | length[1];
+        }
     } FRAME_t;
 
+    RCMP();
+    virtual ~RCMP();
+
+    rapidjson::SchemaDocument * loadSchema(const char * schema);
+
+    /**
+     * parse RCMP signaling from given buffer
+     * 
+     * pBuf: raw buffer
+     * len: data lenth
+     * 
+     * return: RCMP signaling object (rapidjson DOM object)
+     */
+    int parse(void *pBuf, int len, rapidjson::Document &doc);
+
+protected:
+    rapidjson::SchemaDocument * mpSchema_Sig;
+    rapidjson::SchemaDocument * mpSchema_SigLogin;
+    rapidjson::SchemaDocument * mpSchema_SigLoginResp;
+    rapidjson::SchemaDocument * mpSchema_SigLogout;
+    rapidjson::SchemaDocument * mpSchema_SigLogoutResp;
+    rapidjson::SchemaDocument * mpSchema_SigPing;
+    rapidjson::SchemaDocument * mpSchema_SigPong;
+
+    rapidjson::SchemaValidator * mpSchemaValidator_Sig;
+    rapidjson::SchemaValidator * mpSchemaValidator_SigLogin;
+    rapidjson::SchemaValidator * mpSchemaValidator_SigLoginResp;
+    rapidjson::SchemaValidator * mpSchemaValidator_SigLogout;
+    rapidjson::SchemaValidator * mpSchemaValidator_SigLogoutResp;
+    rapidjson::SchemaValidator * mpSchemaValidator_SigPing;
+    rapidjson::SchemaValidator * mpSchemaValidator_SigPong;
+
+    int verifyFrame(FRAME_t *pFrame, int len);
+    bool verifySig(rapidjson::Document &doc);
 };
 
 } // namespace interface
