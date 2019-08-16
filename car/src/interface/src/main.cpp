c15d4d6d0d4443a725f51018831953fbb6444964
Liu Yu <source@liuyu.com>
2019-08-16 09:47:28 +0800

    update debug output

--- car/src/interface/src/main.cpp
+++ car/src/interface/src/main.cpp
@@ -26,20 +26,16 @@ int main(int argc, char **argv)
     ros::NodeHandle nh;
 
     // init interface
-    ROS_DEBUG("Debug: init interface.");
+    ROS_DEBUG("init interface.");
     Interface::init(nh);
 
     // init uds
-    ROS_DEBUG("Debug: init uds.");
+    ROS_DEBUG("init uds.");
     UDS uds(Interface::onSignaling);
     uds.start(UDS_URI);
 
-    ROS_INFO("Info: ROS Car Interface Ready.");
-    while (true)
-    {
-        ROS_INFO("ROS Car Interface Sleeping...");
-        this_thread::sleep_for(chrono::seconds(29));
-    }
+    ROS_INFO("ROS Car Interface Ready.");
+    uds.join();
 
     return 0;
 }
