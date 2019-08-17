663e98d1f44bbc50e7045b4489e7fa52f2531848
Liu Yu <source@liuyu.com>
2019-08-17 14:57:21 +0800

    create new service: Move

--- car/src/pilot/src/pilot.cpp
+++ car/src/pilot/src/pilot.cpp
@@ -1,5 +1,9 @@
 #include "ros/ros.h"
+#include "roscar_common/error.h"
 #include "pilot/Info.h"
+#include "pilot/Move.h"
+
+using namespace roscar::car;
 
 bool info(pilot::Info::Request &req,
           pilot::Info::Response &res)
@@ -10,18 +14,33 @@ bool info(pilot::Info::Request &req,
   res.type = "string of type";
   res.name = "string of name";
 
-  ROS_DEBUG("sending info back");
+  return true;
+}
+
+bool move(pilot::Move::Request &req,
+          pilot::Move::Response &res)
+{
+  ROS_DEBUG("move [angle: %.2f, power: %.2f, duration: %.2f]",
+            req.angle,
+            req.power,
+            req.duration);
+
+  res.err_code = roscar_common::SUCCESS;
+  res.err_msg = "";
 
   return true;
 }
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "pilot_node");
+  ros::init(argc, argv, "pilot");
+
   ros::NodeHandle n;
 
-  ros::ServiceServer service = n.advertiseService("info", info);
-  ROS_INFO("Pilot Node Ready.");
+  ros::ServiceServer srv_info = n.advertiseService("info", info);
+  ros::ServiceServer srv_move = n.advertiseService("move", move);
+
+  ROS_INFO("Pilot Ready.");
   ros::spin();
 
   return 0;
